34b46645a2a893f7bb1bd1739ea56433afcab9f7
codemasher <smiley@chillerlan.net>
2021-09-03 18:28:17 +0200

    :shower: self -> $this

--- tests/EnvTest.php
+++ tests/EnvTest.php
@@ -46,58 +46,58 @@ class EnvTest extends TestCase{
 
 	public function testAddEnv():void{
 		$this->dotenv->addEnv(__DIR__, '.env_test');
-		self::assertNull($this->dotenv->get('foo'));
+		$this::assertNull($this->dotenv->get('foo'));
 
 		$this->dotenv->addEnv(__DIR__, '.another_env', true, ['FOO']); // case sensitive here!
-		self::assertSame('BAR', $this->dotenv->get('foo'));
+		$this::assertSame('BAR', $this->dotenv->get('foo'));
 	}
 
 	public function testLoadGet():void{
 		$this->dotenv->load(['VAR']);
 
-		self::assertFalse(isset($_ENV[42])); // numerical keys shouldn't exist in globals
+		$this::assertFalse(isset($_ENV[42])); // numerical keys shouldn't exist in globals
 
-		self::assertNotEmpty($_ENV); // we're in global mode
-		self::asserttrue(isset($_ENV['VAR']));
+		$this::assertNotEmpty($_ENV); // we're in global mode
+		$this::asserttrue(isset($_ENV['VAR']));
 
-		self::assertSame('test', $_ENV['VAR']);
-		self::assertSame('test', $this->dotenv->get('var'));
-		self::assertSame('test', $this->dotenv->get('VAR'));
-		self::assertSame('test', $this->dotenv->var);
-		self::assertSame('test', $this->dotenv->VAR);
-		self::assertSame($_ENV['VAR'], $this->dotenv->get('VAR'));
-		self::assertSame($_ENV['VAR'], $this->dotenv->VAR);
+		$this::assertSame('test', $_ENV['VAR']);
+		$this::assertSame('test', $this->dotenv->get('var'));
+		$this::assertSame('test', $this->dotenv->get('VAR'));
+		$this::assertSame('test', $this->dotenv->var);
+		$this::assertSame('test', $this->dotenv->VAR);
+		$this::assertSame($_ENV['VAR'], $this->dotenv->get('VAR'));
+		$this::assertSame($_ENV['VAR'], $this->dotenv->VAR);
 
-		self::assertSame('Oh here\'s some silly &%=ä$&/"§% value', $_ENV['TEST']); // stripped comment line
-		self::assertSame('foo'.PHP_EOL.'bar'.PHP_EOL.'nope', $_ENV['MULTILINE']);
+		$this::assertSame('Oh here\'s some silly &%=ä$&/"§% value', $_ENV['TEST']); // stripped comment line
+		$this::assertSame('foo'.PHP_EOL.'bar'.PHP_EOL.'nope', $_ENV['MULTILINE']);
 
-		self::assertSame('Hello World!', $_ENV['VAR3']);
-		self::assertSame('{$VAR1} $VAR2 {VAR1}', $_ENV['VAR4']); // not resolved
+		$this::assertSame('Hello World!', $_ENV['VAR3']);
+		$this::assertSame('{$VAR1} $VAR2 {VAR1}', $_ENV['VAR4']); // not resolved
 	}
 
 	public function testSetUnsetClear():void{
 		$this->dotenv->load();
 
-		self::assertTrue(isset($_ENV['TEST']));
-		self::assertTrue(isset($this->dotenv->TEST));
+		$this::assertTrue(isset($_ENV['TEST']));
+		$this::assertTrue(isset($this->dotenv->TEST));
 		unset($this->dotenv->TEST);
-		self::assertFalse(isset($_ENV['TEST']));
-		self::assertFalse($this->dotenv->get('test'));
-		self::assertFalse($this->dotenv->test);
+		$this::assertFalse(isset($_ENV['TEST']));
+		$this::assertFalse($this->dotenv->get('test'));
+		$this::assertFalse($this->dotenv->test);
 
 		// generic
 		$this->dotenv->set('TESTVAR', 'some value: ${var3}');
-		self::assertSame('some value: Hello World!', $_ENV['TESTVAR']);
-		self::assertSame('some value: Hello World!', $this->dotenv->get('TESTVAR'));
+		$this::assertSame('some value: Hello World!', $_ENV['TESTVAR']);
+		$this::assertSame('some value: Hello World!', $this->dotenv->get('TESTVAR'));
 
 		// magic
 		$this->dotenv->TESTVAR = 'some other value: ${var3}';
-		self::assertSame('some other value: Hello World!', $_ENV['TESTVAR']);
-		self::assertSame('some other value: Hello World!', $this->dotenv->TESTVAR);
+		$this::assertSame('some other value: Hello World!', $_ENV['TESTVAR']);
+		$this::assertSame('some other value: Hello World!', $this->dotenv->TESTVAR);
 
 		$this->dotenv->clear();
 
-		self::assertSame([], $_ENV);
+		$this::assertSame([], $_ENV);
 	}
 
 }
--- tests/EnvTestNonGlobal.php
+++ tests/EnvTestNonGlobal.php
@@ -23,42 +23,42 @@ class EnvTestNonGlobal extends EnvTest{
 	public function testLoadGet():void{
 		$this->dotenv->load(['VAR']);
 
-		self::assertFalse(isset($_ENV[42])); // numerical keys shouldn't exist in globals
+		$this::assertFalse(isset($_ENV[42])); // numerical keys shouldn't exist in globals
 
 
-		self::assertSame([], $_ENV); // we're in non-global mode
-		self::assertFalse(isset($_ENV['VAR']));
+		$this::assertSame([], $_ENV); // we're in non-global mode
+		$this::assertFalse(isset($_ENV['VAR']));
 
-		self::assertSame('test', $this->dotenv->get('var'));
-		self::assertSame('test', $this->dotenv->get('VAR'));
+		$this::assertSame('test', $this->dotenv->get('var'));
+		$this::assertSame('test', $this->dotenv->get('VAR'));
 
-		self::assertSame('Oh here\'s some silly &%=ä$&/"§% value', $this->dotenv->get('TEST')); // stripped comment line
-		self::assertSame('foo'.PHP_EOL.'bar'.PHP_EOL.'nope', $this->dotenv->get('MULTILINE'));
+		$this::assertSame('Oh here\'s some silly &%=ä$&/"§% value', $this->dotenv->get('TEST')); // stripped comment line
+		$this::assertSame('foo'.PHP_EOL.'bar'.PHP_EOL.'nope', $this->dotenv->get('MULTILINE'));
 
-		self::assertSame('Hello World!', $this->dotenv->get('VAR3'));
-		self::assertSame('{$VAR1} $VAR2 {VAR1}', $this->dotenv->get('VAR4')); // not resolved
+		$this::assertSame('Hello World!', $this->dotenv->get('VAR3'));
+		$this::assertSame('{$VAR1} $VAR2 {VAR1}', $this->dotenv->get('VAR4')); // not resolved
 	}
 
 	public function testSetUnsetClear():void{
 		$this->dotenv->load();
 
-		self::assertFalse(isset($_ENV['TEST']));
-		self::assertTrue($this->dotenv->isset('TEST'));
+		$this::assertFalse(isset($_ENV['TEST']));
+		$this::assertTrue($this->dotenv->isset('TEST'));
 		unset($this->dotenv->TEST);
-		self::assertFalse(isset($_ENV['TEST']));
-		self::assertNull($this->dotenv->get('test'));
+		$this::assertFalse(isset($_ENV['TEST']));
+		$this::assertNull($this->dotenv->get('test'));
 
 		// generic
 		$this->dotenv->set('TESTVAR', 'some value: ${var3}');
-		self::assertSame('some value: Hello World!', $this->dotenv->get('TESTVAR'));
+		$this::assertSame('some value: Hello World!', $this->dotenv->get('TESTVAR'));
 
 		// magic
 		$this->dotenv->TESTVAR = 'some other value: ${var3}';
-		self::assertSame('some other value: Hello World!', $this->dotenv->TESTVAR);
+		$this::assertSame('some other value: Hello World!', $this->dotenv->TESTVAR);
 
 		$this->dotenv->clear();
 
-		self::assertSame([], $_ENV);
+		$this::assertSame([], $_ENV);
 	}
 
 }
